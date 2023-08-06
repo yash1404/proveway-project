c64dd667e277eef5ae0bd7670412937f950b785b
yash singh <yashe045@gmail.com>
2023-08-06 21:50:33 +0530

    update the functiionality

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 function toggleSelection(boxNumber) {
-  var selectionDiv = document.getElementById("selection" + boxNumber);
-  var boxDiv = document.getElementById("box" + boxNumber);
-  var radioButton = document.getElementById("radioButton" + boxNumber);
-
-  if (radioButton.checked) {
-    selectionDiv.style.display = "block";
-    boxDiv.style.border = "2px solid #007f61";
-    boxDiv.style.backgroundColor = "#F4FBF9";
-  } else {
-    selectionDiv.style.display = "none";
-   
+    var allSelectionDivs = document.querySelectorAll(".selection");
+    var allBoxDivs = document.querySelectorAll(".box");
+    var allRadioButtons = document.querySelectorAll(".radio-input input[type='radio']");
+  
+    for (var i = 0; i < allSelectionDivs.length; i++) {
+      if (i + 1 === boxNumber) {
+        allSelectionDivs[i].style.display = "block";
+        allBoxDivs[i].style.border = "2px solid #007f61";
+        allBoxDivs[i].style.backgroundColor = "#F4FBF9";
+        allRadioButtons[i].checked = true;
+      } else {
+        allSelectionDivs[i].style.display = "none";
+        allBoxDivs[i].style.border = "0.904px solid #c8c8c8";
+        allBoxDivs[i].style.backgroundColor = "transparent";
+        allRadioButtons[i].checked = false;
+      }
+    }
   }
-}
+  
